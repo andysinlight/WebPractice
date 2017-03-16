8660545315673971a79d6684e28a92c8c47e6dce
Administrator <andysinlight@163.com>
2017-03-16 23:50:16 +0800

    delect

--- BookStore/src/com/yuan/beans/User.java
+++
@@ -1,5 +0,0 @@
-package com.yuan.beans;
-
-public class User {
-
-}
--- BookStore/store.sql
+++
@@ -1,7 +0,0 @@
-create database bookstore;
-use bookstore;
-create table category(
-	id varchar(100) not null primary key,
-	name varchar(100) not null,
-	des varchar(255)
-);
